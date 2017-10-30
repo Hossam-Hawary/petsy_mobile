2d046da39141c5a3f16828e3e93c15df1072eb70
Hossam-Hawary <hossameldeen.hawary@gmail.com>
2017-10-31 01:29:19 +0200

    init translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TranslateService} from '@ngx-translate/core';
+
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -16,7 +18,8 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar
+    , public splashScreen: SplashScreen, private translate: TranslateService) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -31,6 +34,10 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+              // this language will be used as a fallback when a translation isn't found in the current language
+      this.translate.setDefaultLang('en');
+         // the lang to use, if the lang isn't available, it will use the current loader to get them
+      this.translate.use('en');
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Pro } from '@ionic/pro';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -9,26 +13,52 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+  export function createTranslateLoader(http: HttpClient) {
+      return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  }
+  const IonicPro = Pro.init('1430dc61', {
+    appVersion: "0.0.1"
+  });
+
+  export class MyErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    IonicPro.monitoring.handleNewError(err);
+  }
+}
+
+
+const pages :any[]=[
+    HomePage,
+    ListPage
+]
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    ...pages
+
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+            }
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+     ...pages
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: MyErrorHandler }
+
   ]
 })
 export class AppModule {}
